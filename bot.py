2863847a85d43786fa5541f2c7aeac17044d0c51
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-15 11:16:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,3 +30,16 @@ async def start(bot, message):
     )
     
         
+def ai_responses(input_text):
+    user_message = str(input_text).lower()
+    prompt_addition = "ENTER YOUR PROMPT HERE" + user_message
+    openai.api_key = OPEN_AI_KEY
+    bot_output = openai.Completion.create(engine="text-davinci-002",prompt=prompt_addition,temperature=0.7,max_tokens=200,top_p=1,frequency_penalty=0,presence_penalty=0)
+    response = bot_output['choices'][0]['text']
+    return response
+  
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    
+    
+  
