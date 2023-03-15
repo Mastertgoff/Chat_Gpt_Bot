56269e7cb580d4ad09860cb7a34e7c74f351154b
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-15 11:03:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
 
-START_MSG = 
+START_MSG = "Hi Iam Artificially Intelligente Bot Maded By @MLZ_BOTZ Ask Me Anything I will Give Answers Properly"
 
 Client = Client(name="chat-gpt",
              api_id=API_ID,
@@ -16,3 +16,17 @@ Client = Client(name="chat-gpt",
              workers=300
              )
 
+@Client.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    buttons = [[
+        InlineKeyboardButton('Oᴡɴᴇʀ', user_id='1957296068'),
+        InlineKeyboardButton('Gʀᴏᴜᴘ', url='https://t.me/MaSTeR_filims')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(
+        text=START_MSG.format(message.from_user.mention),
+        reply_markup=reply_markup,
+        parse_mode=enums.ParseMode.HTML,
+    )
+    
+        
